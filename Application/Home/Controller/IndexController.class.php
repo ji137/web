eb4a236572be4daed5224e52096f449cd9bcb5ef
azhai2016 <1229991003@qq.com>
2016-10-12 17:34:47 +0800

    20161012

--- Application/Home/Controller/IndexController.class.php
+++ Application/Home/Controller/IndexController.class.php
@@ -8,7 +8,6 @@
 // +----------------------------------------------------------------------
 
 namespace Home\Controller;
-use OT\DataDictionary;
 
 /**
  * 前台首页控制器
@@ -18,20 +17,22 @@ class IndexController extends HomeController {
 
 	//系统首页
     public function index(){
-
-        $category = D('Category')->getTree();
-        $lists    = D('Document')->lists(null);
-        
-        $main_contact =  C('MAIN_CONTACT');
-
-        $this->assign('category',$category);//栏目
-        $this->assign('maincontact',$main_contact);//栏目
-        
-        $this->assign('lists',$lists);//列表
-        $this->assign('page',D('Document')->page);//分页
-
-                 
         $this->display();
     }
+     
+    public function oneday(){
+
+        $this->display();
+    }  
+
+     public function threeday(){
+
+        $this->display();
+    }  
+
+     public function sevenday(){
+
+        $this->display();
+    }  
 
 }
